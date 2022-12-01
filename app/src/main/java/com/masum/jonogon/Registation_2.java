a0b262d8f1f3d597dc118c7be3d27914ceba2558
masumahemd <rimasum01724@gmail.com>
2022-12-01 18:15:36 +0600

    just now i add calender for pick date of birth

--- app/src/main/java/com/masum/jonogon/Registation_2.java
+++ app/src/main/java/com/masum/jonogon/Registation_2.java
@@ -3,20 +3,51 @@ package com.masum.jonogon;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
+import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 public class Registation_2 extends AppCompatActivity {
     Button b1;
+    EditText dob;
+    int year,month,day;
 
     @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registation2);
+
         b1=findViewById(R.id.next2);
+        dob=findViewById(R.id.dob);
+        final Calendar calendar=Calendar.getInstance();
+        dob.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                year=calendar.get(Calendar.YEAR);
+                month=calendar.get(Calendar.MONTH);
+                day=calendar.get(Calendar.DAY_OF_MONTH);
+                DatePickerDialog datePickerDialog= new DatePickerDialog(Registation_2.this, new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                        dob.setText(SimpleDateFormat.getDateInstance().format(calendar.getTime()));
+
+                    }
+                },year,month,day);
+                datePickerDialog.show();
+
+
+
+            }
+        });
         b1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
